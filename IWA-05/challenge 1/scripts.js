f8b9b8168c2162a64796367a326cb02c709d962d
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-21 21:15:55 +0200

    Made some code modifications to scripts.js

--- IWA-05/challenge 1/scripts.js
+++ IWA-05/challenge 1/scripts.js
@@ -1,34 +1,63 @@
-FREE_WARNING = 'Free shipping only applies to single customer orders'
-BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
-NONE_SELECTED = '0'
+const FREE_WARNING = 'Free shipping only applies to single customer orders'
+const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence'
+const NONE_SELECTED = parseInt('0')
 
-if (location = RSA) { shipping === 400 && currency === 'R' }
 
-if location = NAM
-shipping = 600 
-else shipping = 800
+// console.log(FREE_WARNING)
+// console.log(BANNED_WARNIN)
+// console.log(NONE_SELECTED)
 
-shoes = 300 * 1
-toys - 100 * 5
-shirts = 150 * 'NONE_SELECTED'
-batteries 35 * 2
-pens = 5 * 'NONE_SELECTED' 
+let location = 'RSA';
+let shipping = 400;
+let currency = 'R';
+let customers = 1;
 
-shipping = null
-currency = $
 
-if (shoes + batteries + pens + shirts > 1000 &&  ) {
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
-		}
-	}
+if (location = 'RSA') { shipping === 400 && currency === 'R' }
+
+// console.log(location, shipping, currency)
+
+if (location = 'NAM'){ 
+		shipping = 600 
+}else {
+		shipping = 800
 }
 
-if (shipping = 0) && (customers !=== 1) { console.log(WARNING) }
+// console.log(location, shipping)
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-customers = '1'
-const location = 'RSA'
-currency = null
+let shoes = 300 * 1
+let toys = 100 * 5
+let shirts = 150 * parseInt('0')
+let batteries = 35 * 2
+let pens = 5 * parseInt('0')
+
+// console.log(shoes, toys, shirts, batteries, pens);
+
+/* The only code to sort out
+shipping = null
+currency = $ 
+*/
+
+// console.log(shipping, currency)
+
+let result =  (shoes + batteries + pens + shirts >= 1000 && shipping ? 'free shipping' : 'no free shipping') 
+
+
+customers = (location = 'NAM' && customers < 2 ? 'free shipping' : 'WARNING')  
+
+
+
+		if (location = 'RSA') {
+			shipping = 0 || shipping
+
+			// console.log('free shipping')
+		}else {
+			// console.log('no free shipping')
+		}
+				
+		 	
+
+if (shipping = 0 && customers !== 1) { console.log(WARNING) }
+
+location === 'NK' ? console.warn('WARNING') : console.log('Price: ', currency + shoes + batteries + pens + shirts + shipping)
