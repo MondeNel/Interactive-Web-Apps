b4b9dad28f1a3b94b5369d6dd2274b39d28f3597
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-22 11:04:45 +0200

    modified challenge 1 of IWA-05

--- IWA-05/challenge 1/scripts.js
+++ IWA-05/challenge 1/scripts.js
@@ -3,20 +3,21 @@ const BANNED_WARNIN = 'Unfortunately we do not ship to your country of residence
 const NONE_SELECTED = parseInt('0')
 
 
-let location = 'RSA';
-let shipping = 400;
-let currency = 'R';
-let customers = 1;
-
+let location = 'RSA'
+let shipping = 400
+let currency = 'R'
+let customers = 1
+let $ = 600
+let countries = parseInt('800')
 
 if (location = 'RSA') { shipping === 400 && currency === 'R' }
 
-
 if (location = 'NAM'){ 
-		shipping = 600 
+		shipping = $ 
 }else {
-		shipping = 800
+		shipping = countries
 }
+
 
 
 
@@ -26,21 +27,30 @@ let shirts = 150 * parseInt('0')
 let batteries = 35 * 2
 let pens = 5 * parseInt('0')
 
-
+shipping = null
 
 if (shoes + batteries + pens + shirts <= 1000) {
-
-	if (location = 'NAM' && customers < 2) {
-
-		if (location = 'RSA'){
-			shipping = 0 || shipping 
-		}
-	}  
-		
+	console.log('Free shipping')
+}else {
+	console.log('No free shipping')
 }
-	 	
 
-if (shipping = 0 && customers !== 1) { console.log(WARNING) }
+
+if (location = 'NAM' && customers < 2) {
+	console.log('Free shipping')
+}else {
+	console.log('No free shipping')
+}
+
+if (location = 'RSA' && (shipping = 0 !== shipping )){
+	console.log('Free shipping')
+}else {
+	console.log('No free shipping')
+}
+
+if (shipping !== 0 && customers !== 1) { 
+	console.log('WARNING') 
+}
 
 shipping = 400;
 
