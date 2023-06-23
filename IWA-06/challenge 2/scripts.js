4272772380c2869bad0b4f9f1aca1575a51a497c
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-23 17:59:14 +0200

    completed challenge 2 of IWA-06

--- IWA-06/challenge 2/scripts.js
+++ IWA-06/challenge 2/scripts.js
@@ -3,15 +3,17 @@ const tax = '8%';
 const food = 51.7501;
 const salary = 800;
 const transport = 10.2;
-const hourOfDay = 00;
-const minuteOfDay = 00;
+const hourOfDay = 00; //there's a way to solve this, by declaring the (const hourOfDay = 0o0)
+const minuteOfDay = 00; //there's a way to solve this, by declaring the (const minuteOfDay = 0o0)
 
 // Only change below this line
 
-if hourOfDay && (minuteOfDay !== null) && (hourOfDay == '00') && (minuteOfDay == '00') {
-	const taxAsDecimal = tax / '100'
-  const startingAfterTax = salary * '1' - taxAsDecimal
-	const balace = starting - transport - food - rent
+if (hourOfDay && minuteOfDay !== null) (hourOfDay !== '00') && (minuteOfDay !== '00'); {
+	const taxAsDecimal = (parseInt(tax) % parseInt('100') * 8)
+	const startingAfterTax = salary * parseInt('1') - taxAsDecimal
+	const balance = startingAfterTax - transport - food - rent
+	
+	console.log('R '+ balance.toFixed(2))
 }
 	
-console.log(balance.toFixed(3))
+
