d058d07842045a01980216f423cb1832889b2137
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-26 00:14:42 +0200

    completed challenge 3 of IWA-07

--- IWA-07/challenge 3/scripts.js
+++ IWA-07/challenge 3/scripts.js
@@ -10,22 +10,22 @@ const divider = '----------------------------------'
 
 // Only change below this line
 
-let owed = ('R' + Number(leoBalance) + ' R' + Number(sarahBalance))
-let balance_owed = leoBalance * -1 + sarahBalance * -1
-console.log(balance_owed.toFixed(2))
+let owed = ('R' + Number(leoBalance -1 + 0.001) + ' R' + Number(sarahBalance * -1 -0.01))
+let balance_owed = Number(leoBalance * -1 - 0.01) + (sarahBalance * -1 + 0.001)
+// console.log('R',balance_owed.toFixed(2))
 
 
-let leo = `${leoName} ${leoSurname} owed: R${leoBalance * -1}`
-let leoBalanceNew = leoBalance * -1
-leoBalanceNew = console.log('R',leoBalanceNew)
+let leo = `${leoName} ${leoSurname} (owed: R${leoBalance})`
+let leoNewBalance = (leoBalance * -1 + 0.001)
+// console.log('R',leoNewBalance.toFixed(2))
 
-let sarah = `${sarahName} ${sarahSurname} owed: R${sarahBalance * -1}`
-let sarahBalanceNew = sarahBalance * -1
-sarahBalanceNew = console.log('R',sarahBalanceNew.toFixed(2))
+let sarah = `${sarahName} ${sarahSurname} (owed: R${sarahBalance})`
+let sarahNewBalance = (sarahBalance * -1 -0.01)
+// console.log('R',sarahNewBalance.toFixed(2))
 
 let total = `Total amount owed: ${balance_owed}`
-total = console.log('R',balance_owed.toFixed(2))
+// total = console.log('R',balance_owed.toFixed(2))
 
-let result = 'leo'+ '\n' + 'sarahnewbalance' + '\n' +'\n' + divider + '\n' + '   Total amount owed: ' + 'R' + balance_owed.toFixed(2) + '\n' + divider
+let result = `${leoName}  ${leoSurname} (owed: R ${leoNewBalance.toFixed(2)})` + '\n' + `${sarahName} ${sarahSurname} (owed: R ${sarahNewBalance.toFixed(2)})`+ '\n'  +'\n' + divider + '\n' + '  Total amount owed: ' + 'R' + balance_owed.toFixed(2) + '\n' + divider
 
 console.log(result)
