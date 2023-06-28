c728fd7c7fb55ba29cddd1b928d2d54c72bca773
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-28 19:38:56 +0200

    final modifications on code challenge

--- IWA-06/challenge 2/scripts.js
+++ IWA-06/challenge 2/scripts.js
@@ -1,28 +1,31 @@
-const rent = 400;
-const tax = '8%';
-const food = 51.7501;
-const salary = 800;
-const transport = 10.2;
-const hourOfDay = 00; //there's a way to solve this, by declaring the (const hourOfDay = 0o0)
-const minuteOfDay = 00; //there's a way to solve this, by declaring the (const minuteOfDay = 0o0)
+personIncome();
 
-// Only change below this line
+	
+function personIncome() {
+	const rent = 400;
+	const tax = '8%';
+	const food = 51.7501;
+	const salary = 800;
+	const transport = 10.2;
+	const hourOfDay = 0; //the previous code contain hourOfDay = 00
+	const minuteOfDay = 0; //the previous code contain minuteOfDay = 00
 
-if  (hourOfDay === undefined && minuteOfDay === undefined); {
-	console.log(hourOfDay, minuteOfDay)
+
+
+	// Only change below this line
+	if (hourOfDay === undefined && minuteOfDay === undefined); {
+		console.log(hourOfDay, minuteOfDay);
+	}
+
+	const taxAsDecimal = (parseInt(tax) % parseInt('100') * 8);
+	console.log(taxAsDecimal);
+
+	const startingAfterTax = salary * parseInt('1') - taxAsDecimal;
+	console.log(startingAfterTax);
+
+	const balance = startingAfterTax - transport - food - rent;
+	console.log(balance);
+
+	console.log(`R ${balance.toFixed(2)}`);
 }
-
-const taxAsDecimal = (parseInt(tax) % parseInt('100') * 8)
-	console.log(taxAsDecimal)
-
-const startingAfterTax = salary * parseInt('1') - taxAsDecimal
-	console.log(startingAfterTax)
-
-const balance = startingAfterTax - transport - food - rent
-	console.log(balance)
-	
-console.log('R '+ balance.toFixed(2))
-
-	
-
 
