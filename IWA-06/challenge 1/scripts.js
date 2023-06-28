5720ee466fa6525a270d290eb1b6711a60314f31
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-28 15:36:11 +0200

    modified the code, chnaged the logic for number

--- IWA-06/challenge 1/scripts.js
+++ IWA-06/challenge 1/scripts.js
@@ -2,9 +2,11 @@ const primaryPhone = 'O748105141'
 const secondaryPhone = '0219131568'
 
 // Only change below this line
+let number;
 
-const primaryValid = typeof number == primaryPhone 
-const secondaryValid = typeof number !== secondaryPhone
+const primaryValid = (number !== parseInt(primaryPhone) ? true : false)
+const secondaryValid = (number !==  parseInt(secondaryPhone) ? true : false)
+
 
 console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
