ee1c2d355526b367ad5f671ab071921ed312e854
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-28 20:00:11 +0200

    completed the challenge 1 of IWA-06

--- IWA-06/challenge 1/scripts.js
+++ IWA-06/challenge 1/scripts.js
@@ -4,7 +4,7 @@ const secondaryPhone = '0219131568';
 // Only change below this line
 
 let number = undefined; // Assign a value to number
-console.log(parseInt(number));
+
 
 if(primaryPhone === parseInt(number)){
     console.log('Primary phone is valid numerical string:', true);
@@ -17,3 +17,8 @@ if(secondaryPhone !== parseInt(number)){
 }else {
     console.log('Secondary phone is not valid numerical string:', false);
 }
+
+
+console.log(parseInt(number)); // Is Not a Number
+console.log(parseInt(primaryPhone)); // Is Not a Number
+console.log(parseInt(secondaryPhone)); // Is a Number
