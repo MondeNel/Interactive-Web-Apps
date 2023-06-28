0298c68a0ae74d08fae1684e14aa7a6e65119c3e
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-28 21:16:22 +0200

    completed modification of code on challenge 3 of IWA-07

--- IWA-07/challenge 3/scripts.js
+++ IWA-07/challenge 3/scripts.js
@@ -6,26 +6,26 @@ const sarahName = 'Sarah    '
 const sarahSurname = 'Kleinhans'
 const sarahBalance = '-4582.21000111'
 
-const divider = '----------------------------------'
+const divider = '----------------------------------';
 
 // Only change below this line
 
-let owed = ('R' + Number(leoBalance -1 + 0.001) + ' R' + Number(sarahBalance * -1 -0.01))
-let balance_owed = Number(leoBalance * -1 - 0.01) + (sarahBalance * -1 + 0.001)
-// console.log('R',balance_owed.toFixed(2))
+const owed = parseInt(leoBalance) + parseInt(sarahBalance);
+// console.log(`R ${owed.toFixed(2)}`);
 
 
-let leo = `${leoName} ${leoSurname} (owed: R${leoBalance})`
-let leoNewBalance = (leoBalance * -1 + 0.001)
-// console.log('R',leoNewBalance.toFixed(2))
+const leo = `${leoName} ${leoSurname} Owed: R${leoBalance  * - 1}`;
+// console.log(leo)
 
-let sarah = `${sarahName} ${sarahSurname} (owed: R${sarahBalance})`
-let sarahNewBalance = (sarahBalance * -1 -0.01)
-// console.log('R',sarahNewBalance.toFixed(2))
 
-let total = `Total amount owed: ${balance_owed}`
-// total = console.log('R',balance_owed.toFixed(2))
+const sarah = `${sarahName} ${sarahSurname} Owed: R${parseInt(sarahBalance  * - 1)}`;
+// console.log(sarah)
 
-let result = `${leoName}  ${leoSurname} (owed: R ${leoNewBalance.toFixed(2)})` + '\n' + `${sarahName} ${sarahSurname} (owed: R ${sarahNewBalance.toFixed(2)})`+ '\n'  +'\n' + divider + '\n' + '  Total amount owed: ' + 'R' + balance_owed.toFixed(2) + '\n' + divider
 
-console.log(result)
+const total = `Total amount owed: R ${owed * - 1}`;
+// console.log(total)
+
+
+const result = leo + '\n' + sarah  + '\n' + divider + '\n' + `   Total amount owed: R ${owed.toFixed(2) * - 1}` + '\n' + divider;
+
+console.log(result);
