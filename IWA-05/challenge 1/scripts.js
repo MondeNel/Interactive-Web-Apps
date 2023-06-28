b179e5312ef58a35dd272b61ac4eb58fccf16e94
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-28 12:08:19 +0200

    modified the code for better logic reading

--- IWA-05/challenge 1/scripts.js
+++ IWA-05/challenge 1/scripts.js
@@ -8,16 +8,24 @@ let shipping = 400
 let currency = 'R'
 let customers = 1
 let $ = 600
-let countries = parseInt('800')
+let otherCountries = parseInt('800')
 
 if (location = 'RSA') { shipping === 400 && currency === 'R' }
+ console.log(currency + shipping + ' shipping for RSA')
+
 
 if (location = 'NAM'){ 
-		shipping = $ 
-}else {
-		shipping = countries
+	shipping = $
+
+}else if (location = otherCountries){
+	shipping = otherCountries
+
+}else if (location = 'NK') {
+	shipping = console.log("We don't ship to North Korea!")
 }
 
+console.log('$'+ $ + ' shipping for NAM');
+console.log('$'+ otherCountries + ' shipping for other countries');
 
 
 
@@ -27,7 +35,6 @@ let shirts = 150 * parseInt('0')
 let batteries = 35 * 2
 let pens = 5 * parseInt('0')
 
-shipping = null
 
 if (shoes + batteries + pens + shirts <= 1000) {
 	console.log('Free shipping')
@@ -35,14 +42,13 @@ if (shoes + batteries + pens + shirts <= 1000) {
 	console.log('No free shipping')
 }
 
-
 if (location = 'NAM' && customers < 2) {
 	console.log('Free shipping')
 }else {
 	console.log('No free shipping')
 }
 
-if (location = 'RSA' && (shipping = 0 !== shipping )){
+if (location = 'RSA' && (shipping = 0 || shipping )){
 	console.log('Free shipping')
 }else {
 	console.log('No free shipping')
@@ -52,6 +58,8 @@ if (shipping !== 0 && customers !== 1) {
 	console.log('WARNING') 
 }
 
-shipping = 400;
+
+
+// shipping = 400
 
 location === 'NK' ? console.warn('WARNING') : console.log('Price: ' + currency, shoes + batteries + pens + shirts +  toys + shipping)
