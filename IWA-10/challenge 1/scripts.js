5f6d565f63b1d29d1a1ba69a96ec94360a808f3f
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-29 10:52:33 +0200

    modified the code structure for better reading

--- IWA-10/challenge 1/scripts.js
+++ IWA-10/challenge 1/scripts.js
@@ -1,88 +1,87 @@
-const currentYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear();
 
 const holidays = {
-    0: {
-        id: 0,
-        name: 'Day of Reconciliation',
-        date: `16 December ${currentYear}`,
-    },
-    1: {
-        id: 1,
-        name: 'Workers Day',
-        date: new Date(`1 April ${currentYear}`),
-    },
-    2: {
-        id: 2,
-        name: 'Day of Goodwill',
-        date: new Date(`26 December ${currentYear}`),
-    },
-    3: {
-        id: 3,
-        name: 'New Year Day',
-        date: new Date(`1 January ${currentYear}`),
-    },
-    4: {
-        id: 4,
-        name: 'Womens Day',
-        date: new Date(`9 August ${currentYear}`),
-    },
-    5: {
-        id: 5,
-        name: 'Heritage Day',
-        date: new Date(`24 September ${currentYear}`),
-    },
-    6: {
-        id: 6,
-        name: 'Christmas Day',
-        date: new Date(`25 December ${currentYear} 13:25`),
-    },
-    7: {
-        id: 7,
-        name: 'Youth Day',
-        date: new Date(`16 June ${currentYear}`),
-    },
-    8: {
-        id: 8,
-        name: 'Human Rights Day',
-        date: new Date(`21 March ${currentYear}`)
-    },
-}
+  0: {
+    id: 0,
+    name: "Day of Reconciliation",
+    date: `16 December ${currentYear}`,
+  },
+  1: {
+    id: 1,
+    name: "Workers Day",
+    date: new Date(`1 April ${currentYear}`),
+  },
+  2: {
+    id: 2,
+    name: "Day of Goodwill",
+    date: new Date(`26 December ${currentYear}`),
+  },
+  3: {
+    id: 3,
+    name: "New Year Day",
+    date: new Date(`1 January ${currentYear}`),
+  },
+  4: {
+    id: 4,
+    name: "Womens Day",
+    date: new Date(`9 August ${currentYear}`),
+  },
+  5: {
+    id: 5,
+    name: "Heritage Day",
+    date: new Date(`24 September ${currentYear}`),
+  },
+  6: {
+    id: 6,
+    name: "Christmas Day",
+    date: new Date(`25 December ${currentYear} 13:25`),
+  },
+  7: {
+    id: 7,
+    name: "Youth Day",
+    date: new Date(`16 June ${currentYear}`),
+  },
+  8: {
+    id: 8,
+    name: "Human Rights Day",
+    date: new Date(`21 March ${currentYear}`),
+  },
+};
 
-const christmas = 6
-const futureId = 9
+const christmas = 6;
+const futureId = 9;
 
 // Do not change code above this comment
 
-if(holidays == futureId){
-    console.log(futureId)
-}else {
-   console.log(`ID ${futureId} not created yet`) 
+if (holidays == futureId) {
+  console.log(futureId);
+} else {
+  console.log(`ID ${futureId} not created yet`);
 }
 
+const copied = holidays[6];
+console.log(copied);
 
-let copied = holidays[6]
-console.log(copied)
+copied.name = { name: "X-mas Day" };
+console.log(copied);
 
-copied = { name: 'X-mas Day' }
-console.log(copied)
+const correctDate = new Date(`25 December ${currentYear} 00:00:00`);
+console.log(correctDate);
 
-let correctDate = (new Date(`25 December ${currentYear} 00:00`));
-console.log(correctDate)
+const correctHours = correctDate.getHours();
+console.log(`${correctHours} Hours`);
 
-let correctHours = correctDate.getHours() 
-console.log(correctHours)
-
-let correctMinutes = correctDate.getMinutes() 
-console.log(correctMinutes)
-
-let isEarlier = copied.date < holidays[6].date
-console.log('New date is earlier:', isEarlier)
+const correctMinutes = correctDate.getMinutes();
+console.log(`${correctMinutes} minutes`);
 
 
-if (isEarlier) copied.date = correctDate
-console.log('ID change:', holidays[christmas].id != copied.id || copied.id)
-console.log('Name change:', holidays[christmas].name != copied.name || copied.name)
-console.log('Date change:', holidays[christmas].date != copied.date || copied.date)
+const isEarlier = copied.date <= holidays[6].date;
+console.log("New date is earlier:", isEarlier);
+
+if (isEarlier) copied.date == correctDate;
+console.log("ID change:", holidays[christmas].id != copied.id || copied.id);
+console.log("Name change:",holidays[christmas].name != copied.name || copied.name);
+console.log("Date change:",holidays[christmas].date != copied.date || copied.date);
 
 // const firstHolidayTimestamp = Math.min(
 //     holidays[0].date.getTime,
@@ -95,7 +94,6 @@ console.log('Date change:', holidays[christmas].date != copied.date || copied.da
 //     holidays[7].date.getTime,
 //     holidays[8].date.getTime,
 // )
-
 
 // const lastHolidayTimestamp = Math.max(
 //     holidays[0].date.getTime,
