3f99edd851219dac7dad724043f34770eec212be
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-06-30 16:56:54 +0200

    managed to get the data of the html attributes

--- IWA-11/challenge 1/scripts.js
+++ IWA-11/challenge 1/scripts.js
@@ -1,34 +1,78 @@
 // script.js
 
-const 1-root = document(order1),
-const 1-biscuits: document(biscuits),
-const 1-donuts: document(donuts),
-const 1-pancakes: document(pancakes),
-const 1-status: document(status)
 
-const 2-root = document(order2),
-const 2-biscuits: document(biscuits),
-const 2-donuts: document(donuts),
-const 2-pancakes: document(pancakes),
-const 2-status: document(status)
 
-const 3-root = document(order3),
-const 3-biscuits: document(biscuits),
-const 3-donuts: document(donuts),
-const 3-pancakes: document(pancakes),
-const 3-status: document(status)
+// Order 1
 
-1-biscuits= 1-root.biscuits,
-1-donuts = 1-root.donuts,
-1-pancakes = 1-root.pancakes,
-1-status = 1-root.status ? Delivered : Pending
+const root1 = document.querySelector('[data-key="order1"]');
+console.log(root1);
 
-2-biscuits= 2-root.biscuits,
-2-donuts = 2-root.donuts,
-2-pancakes = 2-root.pancakes,
-2-status = 2-root.status ? Delivered : Pending
 
-3-biscuits= 3-root.biscuits,
-3-donuts = 3-root.donuts,
-3-pancakes = 3-root.pancakes,
-3-status = 3-root.status ? Delivered : Pending
+const biscuits1 = document.querySelector(".biscuits");
+console.log(biscuits1);
+
+const donuts1 = document.querySelector(".donuts");
+console.log(donuts1);
+
+
+const pancakes1 = document.querySelector(".pancakes");
+console.log(pancakes1);
+
+const status1 =  document.querySelector(".status");
+console.log(status1);
+
+
+
+// Order 2
+
+const root2 = document.querySelector('[data-key="order2"]');
+console.log(root2);
+
+const biscuits2 = document.querySelector(".biscuits");
+console.log(biscuits2);
+
+const donuts2 = document.querySelector(".donuts");
+console.log(donuts2);
+
+
+const pancakes2 = document.querySelector(".pancakes");
+console.log(pancakes2);
+
+const status2 =  document.querySelector(".status");
+console.log(status2);
+
+
+// Order 3
+
+const root3 = document.querySelector('[data-key="order3"]');
+console.log(root3);
+
+const biscuits3 = document.querySelector(".biscuits");
+console.log(biscuits3);
+
+const donuts3 = document.querySelector(".donuts");
+console.log(donuts3);
+
+
+const pancakes3 = document.querySelector(".pancakes");
+console.log(pancakes3);
+
+const status3 =  document.querySelector(".status");
+console.log(status3);
+
+
+
+// 1-biscuits= 1-root.biscuits,
+// 1-donuts = 1-root.donuts,
+// 1-pancakes = 1-root.pancakes,
+// 1-status = 1-root.status ? Delivered : Pending
+
+// 2-biscuits= 2-root.biscuits,
+// 2-donuts = 2-root.donuts,
+// 2-pancakes = 2-root.pancakes,
+// 2-status = 2-root.status ? Delivered : Pending
+
+// 3-biscuits= 3-root.biscuits,
+// 3-donuts = 3-root.donuts,
+// 3-pancakes = 3-root.pancakes,
+// 3-status = 3-root.status ? Delivered : Pending
