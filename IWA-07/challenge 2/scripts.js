d783e6b0bfcbd5eb5f84aee7fce7da5872a6a99a
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-07-03 11:44:25 +0200

    we check for the first truthy value

--- IWA-07/challenge 2/scripts.js
+++ IWA-07/challenge 2/scripts.js
@@ -1,22 +1,9 @@
 const nickName = "Timmy";
 const firstName = "Timothy";
 
-// if (nickName === "Timmy" || firstName !== "Timothy") {
-//     console.log(`Good Morning, ${nickName}!`);
-// } else {
-//     console.log(`Good Morning, ${firstName}!`);
-// }
-
-/**
- * Another expression if nickName was not assigned a value.
- */
-
-const nicknameExample = null; //  This is an intentional absence of a value.
-const firstnameExample = "Timothy";
-
-// Underfined is the absence of a meaningful value
-if(nicknameExample === undefined && firstnameExample === "Timothy") {
-    console.log(console.log(`Good Morning, ${nicknameExample}!`));
-}else {
-    console.log(`Good Morning, ${firstnameExample}!`);
+// the OR operator checks for the first "truthy" value.
+if (nickName === "Timmy" || firstName === "Timothy") {
+    console.log(`Good Morning, ${nickName}!`);
+} else {
+    console.log(`Good Morning, ${firstName}!`);
 }
