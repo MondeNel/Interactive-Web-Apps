d8843fa97f0d3b36dfb970589a444672a8101261
Monde Nel <132911894+MondeNel@users.noreply.github.com>
2023-07-03 11:36:18 +0200

    corrected the expressiion of null and undefined

--- IWA-07/challenge 2/scripts.js
+++ IWA-07/challenge 2/scripts.js
@@ -1,11 +1,11 @@
 const nickName = "Timmy";
 const firstName = "Timothy";
 
-if (nickName === "Timmy" || firstName !== "Timothy") {
-    console.log(`Good Morning, ${nickName}!`);
-} else {
-    console.log(`Good Morning, ${firstName}!`);
-}
+// if (nickName === "Timmy" || firstName !== "Timothy") {
+//     console.log(`Good Morning, ${nickName}!`);
+// } else {
+//     console.log(`Good Morning, ${firstName}!`);
+// }
 
 /**
  * Another expression if nickName was not assigned a value.
@@ -14,7 +14,8 @@ if (nickName === "Timmy" || firstName !== "Timothy") {
 const nicknameExample = null; //  This is an intentional absence of a value.
 const firstnameExample = "Timothy";
 
-if(nicknameExample === "Timmy" || nicknameExample === undefined) {
+// Underfined is the absence of a meaningful value
+if(nicknameExample === undefined && firstnameExample === "Timothy") {
     console.log(console.log(`Good Morning, ${nicknameExample}!`));
 }else {
     console.log(`Good Morning, ${firstnameExample}!`);
