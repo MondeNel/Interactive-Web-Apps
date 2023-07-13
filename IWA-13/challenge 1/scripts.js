6d81b767ef08926483e3d7185f95330317f76835
Monde Nel <mondenel1996@gmail.com>
2023-07-13 11:24:22 +0200

    completed challenge 1 of IWA-13, but couldn't print out the required results

--- IWA-13/challenge 1/scripts.js
+++ IWA-13/challenge 1/scripts.js
@@ -6,60 +6,41 @@ let calculated = '1'
 
 /**
  * 
- * @param {The variable calculated is not defined within the function. 
- * we will need to either pass it as a parameter or define it within the function for the code to work.} calculated 
- * 
  * The conditional statement typeof calculated is missing a comparison operator.
  * It should be typeof calculated === 'string' to check if calculated is a string.
  */
 
-/** This is an example of a function I wrote myself to compare it to the challenge.
- * 
- * let firstName = "Monde";
 
-  function myFnc (){
-    let lastname = 'Nel';
+const logCalc = (calculated) => { 
     
-    console.log(firstName + " " + lastname);
+// we performed a tenary expression to check if calculated is a numerical string
+const isString = calculated === "string" ?  'numerical-string' : 'not a muerical-string' ; 
+
+// we re-assigned it to a number(Integer), to perform some calculation.
+const calculatedAsNumber = parseInt(calculated);
+
+console.log(calculated === calculatedAsNumber + 1); 
 }
-myFnc(); 
-*/
 
 
-let logCalc = () => { 
-    
-  // we performed a tenary expression to check if calculated is a numerical string
-  let isString = typeof calculated === "string" ?  'numerical-string' : 'not a muerical-string' ; 
 
+const calcUser =  () => { // the function was not defined/assigned properly (const = calcUser () => {}) 
 
-  // we re-assigned it to a number(Integer), to perform some calculation.
-  let calculatedAsNumber = parseInt(calculated);
-
-  console.log(calculated === calculatedAsNumber + 1); 
+  // calculated is a string variable of "1", we changed it into an integer to perform expressions
+  if (parseInt(calculated) <= 2) user = 'John';    
+  if (parseInt(calculated) >= 2) state = 'requesting';
+  if (parseInt(calculated) <= 3) state = 'idle';
 }
-// logCalc();
 
 
-let calcUser =  () => { // the variable was not defined/assigned properly. 
+const checkUser = () => {
 
-  if (calculated.length <= 2) user = 'John';  // logical operator less than or  equal too.
-  if (calculated.length >= 2) state = 'requesting';
-  if (calculated.length >= 3) state = 'idle';
-
-  // console.log(user);
-  // console.log(state);
-  // console.log(state);
-}
-// calcUser();
-
-let checkUser = () => {
 	if (user && state !== 'requesting') {
-		console.log(`User: ${user} (${calculated})`)
+
+		console.log(`User: ${user} (${calculated})`);
+
 	}
-  // console.log(user);
-  // console.log(state);
 }
-// checkUser();
 
 // // Only allowed to change code above
 
