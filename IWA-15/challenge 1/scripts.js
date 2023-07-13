a16714e8d20935cfa8055eebde3d9df2280448fc
Monde Nel <mondenel1996@gmail.com>
2023-07-13 19:17:26 +0200

    almost done with the challenge

--- IWA-15/challenge 1/scripts.js
+++ IWA-15/challenge 1/scripts.js
@@ -14,10 +14,12 @@ const data = {
  * This approach directly accesses the target values by chaining the index operators ([]) on the data.lists array.
  * Each index corresponds to the desired level of nesting, enabling us to retrieve the values.
  */
+
+
 // old way
-console.log(data.lists[0][1][0]);  
-console.log(data.lists[1][1][0]); 
-console.log(data.lists[2][1][0]); 
+// console.log(data.lists[0][1][0]);  
+// console.log(data.lists[1][1][0]); 
+// console.log(data.lists[2][1][0]); 
 
 
 /**
@@ -26,32 +28,60 @@ console.log(data.lists[2][1][0]);
  * The empty commas (,) are used to skip the first element (string) in each inner array.
  * Then, we access the first element of each nested array using indexing ([0]) to obtain the target values.
  */
+
+
 // new way
-const [[, firstArray], [, secondArray], [, thirdArray]] = data.lists;
-
-console.log(firstArray[0]);
-console.log(secondArray[0]);
-console.log(thirdArray[0]);
 
 
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
+// This array refers to the "string" array. The comma(,) are used to skip the first array.
+const [[One ,], [Two ,], [Three ,]] = data.lists;
 
-const result = []
+console.log(One);
+console.log(Two);
+console.log(Three);
 
+
+
+// This array refers to the Numbers array. The comma(,) are used to skip the second array.
+const [[, ArrayNum1], [, ArrayNum2], [, ArrayNum3]] = data.lists;
+
+console.log(ArrayNum1[0]);
+console.log(ArrayNum2[0]);
+console.log(ArrayNum3[0]);
+
+
+/**
+* The destructuring assignment is used to extract the nested arrays within data.lists and assign their values to first, second, and third.
+* The first element of each nested array is ignored (represented by an empty slot ,) as it is not needed.
+* The values of first, second, and third are logged to the console.
+* The result array is created with the first element of each nested array (first[0], second[0], third[0]).
+* we targeted the array that consists of numbers, because we can't perform experssions with strings. 
+*/
+
+const [[, first], [, second], [, third]] = data.lists;
+
+console.log(first);
+console.log(second);
+console.log(third);
+
+const result = [first[0], second[0], third[0]];
 
 const extractBiggest = () => {
-	if (first[-1] > second[-1]) {
-		return first
-	}
 
-	if (third[-1] < 1) {
-		return second
+	// we are accessing the last element of an array by using [.length - 1]
+
+	if (first[first.length - 1] >= second[second.length - 1]) {
+	  return first;
 	}
-	
-	return third
-}
+  
+	if (third[third.length - 1] === 1) {
+	  return second;
+	}
+  
+	return third;
+  };
+  
+
 
 // Only edit above
 
