7642625e8d14fc383096e759b23d41d0d95f794a
Monde Nel <mondenel1996@gmail.com>
2023-07-13 10:50:44 +0200

    completed challenge 2 of IWA-14

--- IWA-14/challenge 2/scripts.js
+++ IWA-14/challenge 2/scripts.js
@@ -1,30 +1,19 @@
 // script.js
 
-/**
- * The traditional function syntax allows you to define functions with more flexibility, 
- * including the ability to access the 'this' value
- */ 
-
  
 /**
-* we are also assigning a parameter to the function. parameters are the function's arguments.
-* arguments are passed by value
-* The function body (a + b) or (a * b) calculates the sum of a and b and returns the result.
+* Arrow functions are commonly used when you need a concise function definition, 
+* especially for functions that don't have complex logic or require a separate this context.
 */
 
-function add(a, b) {
+const add = (a, b) => {
 	return a + b;
-}
-console.log(add.call(this, 2, 4));
-// The call() method calls a function by passing 'this' and specified values as arguments.
-
-
-function multiply(a, b) {
+  };
+ 
+  
+  const multiply = (a, b) => {
 	return a * b;
-} 
-
-console.log(multiply.call(this, 2, 2));
-// The call() method calls a function by passing 'this' and specified values as arguments.
+  };
 
 
 
@@ -33,16 +22,27 @@ console.log(multiply.call(this, 2, 2));
  * It allows you to access properties and methods within an object.
  */
 
-// The add function is called using this.add with the values of a and b retrieved from the internal object 
-// (this.internal.a and this.internal.b).
-// the internal function returns 'this'.
-// By using the this.internal, we are referencing to the object to access the values within the object.
+
+
+/**
+ * The add function is called using this.add with the values of a and b retrieved from the internal object 
+ * (this.internal.a and this.internal.b).
+ * he internal function returns 'this'.
+ * By using the this.internal, we are referencing to the object to access the values within the object.
+ */
 
 function internal() {
+
 	const added = this.add(this.internal.a, this.internal.b);
-	this.multiply(this.internal.a, this.internal.b);
+
+	const multiplied = this.multiply(this.internal.a, this.internal.b);
+
+	console.log(added);
+	console.log(multiplied);
+
 	return this;
-}
+  }
+
 
 // // Not allowed to change below this
 
