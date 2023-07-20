22ed78ad579031cacefec3d42f707f39d3bff49b
Monde Nel <mondenel1996@gmail.com>
2023-07-20 15:23:43 +0200

    fix: Correct createData function logic and variable names
    - Set the current date to the 1st day of the month.
    - Calculate the start day of the month using getDay().
    - Generate an array of weeks and days for the calendar.

--- IWA-17/challenge 1/scripts.js
+++ IWA-17/challenge 1/scripts.js
@@ -36,39 +36,47 @@ const createArray = (length) => {
 
     return result
 }
+console.log(createArray());
 
+/**
+* The current variable is now properly created using new Date().
+* The setDate() function is used to set the day of the month to 1.
+* The getDay() function is used to get the day of the week (0 for Sunday, 1 for Monday, etc.).
+* The dayIndex variable is now properly initialized in the inner for-loop, and the loop condition is set to run 7 times (for each day of the week).
+* The createData function is now called at the end, and the result is logged to the console.
+*/
 
-// const createData = () > {
-//     const current = new ()
-//     current.set(1)
+const createData = () => {
+    const current = new Date()
+    current.setDate(1)
 
-//     const startDay = current.get()
-//     const daysInMonth = getDaysInMonth(current)
+    const startDay = current.getDay()
+    const daysInMonth = getDaysInMonth(current)
 
-//     const weeks = createArray(5)
-//     const days = ()
-//     const result = []
+    const weeks = createArray(5)
+    const days = current.getDay()
+    const result = []
 
-//     for(const weekIndex of weeks) {
-//         result.push({
-//             week: weekIndex + 1,
-//             days: []
-//         })
+    for (const weekIndex of weeks) {
+        result.push({
+            week: weekIndex + 1,
+            days: []
+        })
 
-//         for (const of ) {
-//             const day = (dayIndex - startDay) : (weekIndex * 7)  1
-//             const isValid = day > 0 && day <= daysInMonth
+        for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
+            const day = (weekIndex * 7) + dayIndex + 1;
+            const isValid = day > startDay && day <= (startDay + daysInMonth);
 
-//         result[weekIndex].days.push({
-//             dayOfWeek: dayIndex + 1,
-//             value: isValid ? day : '',
-//         })
-//     }
-// }
+            result[weekIndex].days.push({
+                dayOfWeek: dayIndex + 1,
+                value: isValid ? day : '',
+            })
+        }
+    }
 
-// return result
-// }
-
+    return result
+}
+console.log(createData());
 // const addCell = (existing, classString, value) => {
 //     const result = /* html */ `
 //         ${}
