b30ee313a63de7b461556e531b73cd0c152ccecd
Monde Nel <mondenel1996@gmail.com>
2023-07-21 15:14:54 +0200

    completed challenge 1 of IWA-17

--- IWA-17/challenge 1/scripts.js
+++ IWA-17/challenge 1/scripts.js
@@ -31,6 +31,7 @@ const createArray = (length) => {
 }
 console.log(createArray());
 
+
 /**
 * The current variable is now properly created using new Date().
 * The setDate() function is used to set the day of the month to 1.
@@ -47,18 +48,19 @@ const createData = () => {
     const daysInMonth = getDaysInMonth(current)
 
     const weeks = createArray(5)
-    const days = current.getDay()
+    const days = current.getDay(7)
     const result = []
 
     for (const weekIndex of weeks) {
         result.push({
             week: weekIndex + 1,
             days: []
-        })
+        });
 
-        for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
-            const day = (weekIndex * 7) + dayIndex + 1;
-            const isValid = day > startDay && day <= (startDay + daysInMonth);
+
+        for (const dayIndex of weeks) {
+            const day = (weekIndex * 7) + dayIndex - startDay + 1;
+            const isValid = day > 0 && day <= (daysInMonth);
 
             result[weekIndex].days.push({
                 dayOfWeek: dayIndex + 1,
@@ -69,52 +71,56 @@ const createData = () => {
 
     return result
 }
-console.log(createData());
-// const addCell = (existing, classString, value) => {
-//     const result = /* html */ `
-//         ${}
 
-//         <td class="${classString}">
-//             &nbsp;${value}&nbsp;
-//         </td>
-//     `
 
-//     return result
-// }
+const addCell = (existing, classString, value) => {
+    const result = /* html */ `
+        ${existing}
 
-// const createHtml = (data) => {
-//     let result = ''
+        <td class="${classString}">
+            &nbsp;${value}&nbsp;
+        </td>
+    `
 
-//     for (const { week, } of ) {
-//         let inner = ""
-//         inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week}`)
+    return result
+}
 
-//             (const { dayOfWeek, value } of days) {
-//             const isToday = new Date().getDate() === value
-//             const isWeekend = dayOfWeek ===  | dayOfWeek ===
-//             const isAlternate = week % 2 === 0
 
-//             let classString = 'table__cell'
+const createHtml = (data) => {
+    let result = ''
 
-//             if () classString = `${} table__cell_`
-//             if () classString = `${} table__cell_`
-//             if () classString = `${} table__cell_`
-//             inner = addCell()
-//         }
+    for (const { week, days } of data) {
+        let inner = ""
+        inner = addCell(inner, 'table__cell table__cell_sidebar', `Week ${week}`);
 
-//         result = `
-//             ${result}
-//             <tr>${inner}</tr>
-//         `
-//     }
+        for (const { dayOfWeek, value } of days) {
+            const isToday = new Date().getDate() === value;
+            const isWeekend = dayOfWeek === 1 || dayOfWeek === 7;
+            const isAlternate = week % 2 === 0;
 
-//     return result
-// }
+            let classString = 'table__cell'
+
+            if (isToday) classString = `${classString} table__cell_today`
+            if (isWeekend) classString = `${classString} table__cell_weekend`
+            if (isAlternate) classString = `${classString} table__cell_alternate`
+
+            inner = addCell(inner, classString, value);
+        }
+
+        result = `
+            ${result}
+            <tr>${inner}</tr>
+        `
+    }
+
+    return result
+}
+
 
 // // Only edit above
 
-// const current = new Date()
-// document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`
+const current = new Date()
+document.querySelector('[data-title]').innerText = `${MONTHS[current.getMonth()]} ${current.getFullYear()}`
 
-// const data = createData()
-// document.querySelector('[data-content]').innerHTML = createHtml(data)
+const data = createData()
+document.querySelector('[data-content]').innerHTML = createHtml(data)
